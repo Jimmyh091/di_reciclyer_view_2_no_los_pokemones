0869922e82d7f583114e399e18608185afe0e10b
Jimmyh091 <jaimecaballero164@gmail.com>
2025-01-22 20:47:01 +0100

    Ejercicio 2: Mejorado

--- app/src/main/java/com/example/myapplicationasdfasdf/ej1/Ejercicio1.kt
+++ app/src/main/java/com/example/myapplicationasdfasdf/ej1/Ejercicio1.kt
@@ -1,12 +1,13 @@
-package com.example.myapplicationasdfasdf
+package com.example.myapplicationasdfasdf.ej1
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplicationasdfasdf.R
 
-class MainActivity : AppCompatActivity() {
+class Ejercicio1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
